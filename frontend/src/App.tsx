8f7c2cc7da27e9251792a581be382b5669301ea0
trymve <trymve@stud.ntnu.no>
2021-10-28 17:19:40 +0200

    Laget søkefelt med valg av søk

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,7 +7,6 @@ function App() {
   return (
     <div className="App">
       <SearchField></SearchField>
-      <Episode></Episode>
     </div>
   );
 }
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,24 +1,28 @@
-import { applyMiddleware, configureStore, createStore, Store } from "@reduxjs/toolkit";
-import { render } from "@testing-library/react";
-import { Provider } from "react-redux";
-import App from "./App";
-import { EpisodeAction, EpisodeState } from "./store/action/Type";
-import reducer from "./store/Reducer";
-import ReactDom from "react-dom"
-import ReactDOM from "react-dom";
-import React from "react";
-import reportWebVitals from "./reportWebVitals";
-import store from "./store/Store"
+import {
+  applyMiddleware,
+  configureStore,
+  createStore,
+  Store,
+} from '@reduxjs/toolkit'
+import { render } from '@testing-library/react'
+import { Provider } from 'react-redux'
+import App from './App'
+import { EpisodeAction, EpisodeState } from './store/action/Type'
+import reducer from './store/Reducer'
+import ReactDom from 'react-dom'
+import ReactDOM from 'react-dom'
+import React from 'react'
+import reportWebVitals from './reportWebVitals'
+import store from './store/Store'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
-
-const rootElement = document.getElementById("root")
-
+const rootElement = document.getElementById('root')
 
 ReactDOM.render(
-    <Provider store = {store}>
-        <App></App>
-    </Provider>,
-    rootElement
+  <Provider store={store}>
+    <App></App>
+  </Provider>,
+  rootElement
 )
 
 reportWebVitals()
