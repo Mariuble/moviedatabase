625a934c558987f276d541696f70b52a48089461
mariuble <mariuble@stud.ntnu.no>
2021-10-31 22:06:26 +0100

    #11 styled movie elements

--- frontend/src/components/Episode/Movies.tsx
+++ frontend/src/components/Episode/Movies.tsx
@@ -1,8 +1,8 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { useQuery } from '@apollo/client'
-import { } from 'react-bootstrap'
-import { Spinner, Accordion, AccordionItem, Box, AccordionPanel, AccordionButton } from '@chakra-ui/react'
+import { Col, Row } from 'react-bootstrap'
+import { Spinner, Accordion, AccordionItem, Box, AccordionPanel, AccordionButton, Tag, Heading, AccordionIcon } from '@chakra-ui/react'
 import './Episode.css'
 import { SEARCH_MOVIES } from '../../graphql/MovieByTitle'
 
@@ -13,19 +13,28 @@ const Movies = () => {
     console.log(movies)
 
     return (
-        <>
-            <h1>Animes</h1>
+        <Box flex="1" textAlign="center" className='p-1'>
+            <Heading className='m-3'>Animes</Heading>
             {data.movieByTitle.map((movie: any) => (
-                <Accordion allowToggle className='m-1'>
-                    <AccordionItem>
-                        <AccordionButton>{movie.Title}</AccordionButton>
+                <Accordion allowToggle className='m-1' boxShadow="base" rounded='xl'>
+                    <AccordionItem className='p-3 m-2'>
+                        <AccordionButton className='' >
+                            <Col>
+                                <Row>
+                                    <Heading as='h5' size='md' >{movie.Title}</Heading>
+                                </Row>
+                                <Tag className='m-1'>{movie.Type}</Tag>
+                            </Col>
+                            <AccordionIcon />
+                        </AccordionButton>
                         <AccordionPanel>
                             {movie.Description}
                         </AccordionPanel>
                     </AccordionItem>
                 </Accordion>
-            ))}
-        </>
+            ))
+            }
+        </Box>
     )
 }
 
