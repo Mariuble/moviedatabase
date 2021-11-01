12481f67d8076df9059bace57b170e731c14c061
mariuble <mariuble@stud.ntnu.no>
2021-11-01 20:38:07 +0100

    #13 Form variables in states

--- frontend/src/components/MovieForm.tsx
+++ frontend/src/components/MovieForm.tsx
@@ -1,75 +1,78 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Box, Heading, ThemeProvider, theme, FormControl, FormLabel, Input, FormErrorMessage, Button, RadioGroup, Stack, Radio, NumberInput, NumberInputField, NumberInputStepper, NumberIncrementStepper, NumberDecrementStepper, Textarea } from '@chakra-ui/react'
-import { useForm, Controller } from 'react-hook-form'
-
-
 
 // Form for adding Movie
 const MovieForm = () => {
 
-    const { register, handleSubmit, control, formState: { errors, isSubmitting } } = useForm()
+    const [title, setTitle] = useState('')
+    const [type, setType] = useState('Movie')
+    const [episodes, setEpisodes] = useState(0)
+    const [description, setDescription] = useState('This is a default description')
 
-    const onSubmit = (values: any) => {
-        console.log(values)
+    const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault()
+        console.log(title, '|', type, '|', episodes, '|', description)
     }
-
-    // const validateTitle = (value: any) => {
-    //     if (!value) return "Name is required"
-    // }
 
     return (
         <ThemeProvider theme={theme}>
-            <Box w={500} p={4} m="20px auto" border='1px' borderColor="gray.300" borderRadius='xl'>
+            <Box mt={20} w={500} p={4} m="20px auto" border='1px' borderColor="gray.300" borderRadius='xl' boxShadow="lg">
                 <Heading as="h1" size="xl" textAlign="center">
                     Add Anime
                 </Heading>
 
-                {/* handleSubmit will validate the inputs before invoking onSubmit */}
-                <form onSubmit={handleSubmit(onSubmit)}>
-                    <FormControl isInvalid={errors.title}>
+                <form
+                    onSubmit={onSubmit}
+                >
+                    <FormControl isRequired mt={5}>
                         <FormLabel htmlFor='title'>Title</FormLabel>
                         <Input
-                            id='title'
                             placeholder='title'
-                            {...register('title', {
-                                required: 'This is required',
-                                minLength: { value: 4, message: 'Minimum length should be 4' }
-                            })}
+                            onChange={event => setTitle(event.currentTarget.value)}
                         />
-                        <FormErrorMessage>{errors.name && errors.name.message}</FormErrorMessage>
                     </FormControl>
 
-                    <FormLabel mt={5} htmlFor='type'>Type</FormLabel>
-                    <RadioGroup
-                        id='type'
-                        // name='type'
-                        defaultValue='Movie'
-                    >
-                        <Stack spacing={5} direction="row">
-                            <Radio defaultChecked value="Movie">Movie</Radio>
-                            <Radio value="TV">TV</Radio>
-                            <Radio value="OVA">OVA</Radio>
-                        </Stack>
-                    </RadioGroup>
+                    <FormControl isRequired mt={5}>
+                        <FormLabel htmlFor='type'>Type</FormLabel>
+                        <RadioGroup
+                            defaultValue={type}
+                            onChange={setType}
+                        >
+                            <Stack spacing={5} direction="row">
+                                <Radio defaultChecked value="Movie">Movie</Radio>
+                                <Radio value="TV">TV</Radio>
+                                <Radio value="OVA">OVA</Radio>
+                            </Stack>
+                        </RadioGroup>
+                    </FormControl>
 
-                    <FormLabel mt={5} htmlFor='episodes'>Episodes</FormLabel>
-                    <NumberInput defaultValue={15} allowMouseWheel>
-                        <NumberInputField />
-                        <NumberInputStepper>
-                            <NumberIncrementStepper />
-                            <NumberDecrementStepper />
-                        </NumberInputStepper>
-                    </NumberInput>
+                    <FormControl isRequired mt={5}>
+                        <FormLabel htmlFor='episodes'>Episodes</FormLabel>
+                        <NumberInput
+                            defaultValue={15}
+                            allowMouseWheel
+                        >
+                            <NumberInputField onChange={event => setEpisodes} />
+                            <NumberInputStepper>
+                                <NumberIncrementStepper />
+                                <NumberDecrementStepper />
+                            </NumberInputStepper>
+                        </NumberInput>
+                    </FormControl>
 
-                    <FormLabel mt={5} htmlFor='description'>Description</FormLabel>
-                    <Textarea defaultValue={'This is a default description'} placeholder="Write a description" />
+                    <FormControl isRequired mt={5}>
+                        <FormLabel htmlFor='description'>Description</FormLabel>
+                        <Textarea
+                            defaultValue={description}
+                            placeholder="Write a description"
+                            onChange={event => setDescription(event.currentTarget.value)}
+                        />
+                    </FormControl>
 
 
-
-                    <Button mt={10} colorScheme="teal" isLoading={isSubmitting} type="submit">
+                    <Button mt={10} colorScheme="teal" type="submit" width="full">
                         Submit
                     </Button>
-                    {/* {submittedData && <pre>{JSON.stringify(submittedData, null, 2)}</pre>} */}
                 </form>
             </Box>
         </ThemeProvider>
