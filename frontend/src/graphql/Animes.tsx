cac9ffdc061a3ee73bc94864e31c04c2c4610651
trymve <trymve@stud.ntnu.no>
2021-11-02 20:56:12 +0100

    Key up isteden for down

--- frontend/src/graphql/Animes.tsx
+++ frontend/src/graphql/Animes.tsx
@@ -177,7 +177,7 @@ const Animes = () => {
           width='40%'
           placeholder={searchWord}
           variant='outline'
-          onKeyDown={(e) => handleUpdate(e)}
+          onKeyUp={(e) => handleUpdate(e)}
           aria-label='Searchfield'
         />
         <IconButton
